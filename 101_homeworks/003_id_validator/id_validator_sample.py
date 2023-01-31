55a9d422c9347c828943cfe11f327823824d6bca
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-01-31 21:28:12 +0200

    Working with files

--- 101_homeworks/003_id_validator/id_validator_sample.py
+++ 101_homeworks/003_id_validator/id_validator_sample.py
@@ -19,7 +19,7 @@ while True:
             user_choice = input("Please choose:\n1.Gender\n2.Date of birth\n"
                                 "3.Region\n4.Validate\n5.Change ID\n0.Exit\n--> ")
             if user_choice == '1':
-                if user_choice[0] in ['0', '9']:
+                if user_input[0] in ['0', '9']:
                     gender = 'Unknown'
                 elif int(user_input[0]) % 2 == 0:
                     gender = 'Female'
@@ -27,16 +27,17 @@ while True:
                     gender = 'Male'
                 print(f'You are {gender}')
             elif user_choice == '2':
-                if user_choice[1:3] in '12':
+                if user_input[0] in '12':
                     cent = '18'
-                elif user_choice[1:3] in '34':
+                elif user_input[0] in '34':
                     cent = '19'
-                elif user_choice[1:3] in '56':
+                elif user_input[0] in '56':
                     cent = '20'
-                elif user_choice[1:3] in '78':
+                elif user_input[0] in '78':
                     cent = '21'
                 else:
                     cent = 'Unknown'
+                print(f'{user_input[5:7]}.{user_input[3:5]}.{cent}{user_input[1:3]}')
             elif user_choice == '3':
                 r_id = int(user_input[7:10])
                 if r_id in range(1, 11):
@@ -48,11 +49,23 @@ while True:
             elif user_choice == '4':
                 chk1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
                 chk2 = [3, 4, 5, 6, 7, 8, 9, 1, 2, 3]
-                result = user_input[0] * chk1[0] + user_input[1] * chk1[1]
-                if int(user_input[-1]) == result % 11:
-                    print('ID code is valid')
+                result = 0
+                cnt = 0
+                for num in chk1:
+                    result += num * int(user_input[cnt])
+                    cnt += 1
+                if result % 11 == int(user_input[-1]):
+                    print('ID is valid')
                 else:
-                    result = user_choice[0] * chk2[0]
+                    result = 0
+                    cnt = 0
+                    for num in chk2:
+                        result += num * int(user_input[cnt])
+                        cnt += 1
+                    if result % 11 == int(user_input[-1]):
+                        print('ID is valid')
+                    else:
+                        print('ID is not valid')
             elif user_choice == '5':
                 break
             elif user_choice == '0':
