4c0960c2a24cea25fd8977dd6b01236485e928d2
Roman Kutselepa <roman.kutselepa@gmail.com>
2023-02-09 20:57:35 +0200

    RegExp

--- 007_working_with_files/homework.py
+++ 007_working_with_files/homework.py
@@ -1,8 +1,23 @@
-with open('text_files/trimushketera.txt', 'r', encoding='UTF8') as file:
-    data = file.read().lower().replace('.', '').replace(',', '')\
-        .replace('!', '').replace('?', '').replace('"', '').replace('(', '').replace(')', '')
+# with open('text_files/trimushketera.txt', 'r', encoding='UTF8') as file:
+#     data = file.read().lower().replace('.', '').replace(',', '')\
+#         .replace('!', '').replace('?', '').replace('"', '').replace('(', '').replace(')', '')
+#
+# words = data.split()
+# unique = list(set(words))
+# unique.sort()
+#
+# with open('text_files/trimushketera_copy.txt', 'w', encoding='UTF8') as file:
+#     file.write(f'There are {len(words)} words.\n')
+#     file.write(f'There are {len(unique)} unique words.\n')
+#     for word in unique:
+#         file.write(word + f'{word}\n')
 
-words = data.split()
+import re
+with open('text_files/trimushketera.txt', 'r', encoding='UTF8') as file:
+    data = file.read()
+
+pattern = re.compile(r'[^A-Za-zА-я\'\s-]')
+words = pattern.sub('', data).lower().split()
 unique = list(set(words))
 unique.sort()
 
